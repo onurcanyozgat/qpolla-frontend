dc93cf12902c2dd679ce9ef2f690b2c17922ca56
Volkan Ulutaş <volkan.ulutas@esensi.com.tr>
2023-03-20 15:25:38 +0300

    theme

--- App.js
+++ App.js
@@ -2,6 +2,7 @@ import * as React from 'react';
 
 import TabNavScreen from "/src/screen/navigation/NavScreen"; 
 
+
 export default function App() {
   return (
           <TabNavScreen></TabNavScreen>
--- src/screen/navigation/NavScreen.js
+++ src/screen/navigation/NavScreen.js
@@ -1,6 +1,8 @@
 import * as React from 'react';
-import { NavigationContainer } from '@react-navigation/native';
+import { useColorScheme } from 'react-native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { DefaultTheme, DarkTheme, NavigationContainer } from '@react-navigation/native'
+
 
 import QPollScreen from "../qpoll/QPollScreen";
 import CategoryScreen from "../category/CategoryScreen";
@@ -9,9 +11,22 @@ import SettingScreen from "../setting/SettingScreen";
 
 const Tab = createBottomTabNavigator();
 
+const MyTheme = {
+  ...DefaultTheme,
+  colors: {
+    primary: 'rgb(255, 255, 255)',
+    background: 'rgb(195, 224, 229)',
+    card: 'rgb(65, 114, 159)',
+    text: 'rgb(255, 255, 255)',
+    border: 'rgb(39, 68, 114)',
+    notification: 'rgb(255, 69, 58)',
+  },
+};
+
 export default function NavScreen() {
+  const scheme = useColorScheme();
     return (
-      <NavigationContainer>
+      <NavigationContainer theme={scheme === 'dark' ? MyTheme : MyTheme}>
         <Tab.Navigator>
           <Tab.Screen name="qpoll" component={QPollScreen} />
           <Tab.Screen name="Kategoriler" component={CategoryScreen} />
