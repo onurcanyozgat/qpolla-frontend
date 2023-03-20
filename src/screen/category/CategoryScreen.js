d3ad3005eb1d88d9ed2040cf4c289e13c9f91192
Volkan Ulutaş <volkanulutas@hacettepe.edu.tr>
2023-03-20 23:45:07 +0300

    poll service

--- src/screen/category/CategoryScreen.js
+++ src/screen/category/CategoryScreen.js
@@ -1,15 +1,26 @@
-import * as React from 'react';
-import { Text, View } from 'react-native';
-import PollService from '../services/PollService';
+import React, { useState, useEffect } from "react";
+import { Text, View } from "react-native";
+import PollService from "../services/PollService";
 
 export default function CategoryScreen() {
-    const [categoryList, setCategoryList] = React.useState([]);
+  const [categoryList, setCategoryList] = useState([]);
+  const [error, setError] = useState("");
+  const [loaded, setLoaded] = useState(false);
 
-    const getCategoryList = () => {}
+  useEffect(() => {
+    const axiosData = PollService.getCategories();
+    axiosData
+      .then((response) => setCategoryList(response.data))
+      .catch((error) => setError(error.message))
+      .finally(() => setLoaded(true));
+  }, []);
 
-    return (
-      <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-        <Text>Kategoriler!</Text>
-      </View>
-    );
-  }
+  const itemList = categoryList.map((item) => <li>{item}</li>);
+
+  return (
+    <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+      <Text>Kategoriler!</Text>
+      <div>{itemList}</div>
+    </View>
+  );
+}
--- src/screen/services/PollService.js
+++ src/screen/services/PollService.js
@@ -1,14 +1,14 @@
-import axios from 'axios';
+import axios from "axios";
 
-import { API_BASE_URL, API_URL_POLL} from '../constant';
-import authHeader from "./util/AuthHeader"
+import { API_BASE_URL, API_URL_POLL } from "../constant";
+import authHeader from "./util/AuthHeader";
 
 class PollService {
-
-    static getCategories()
-    {
-       return axios.get(API_BASE_URL + API_URL_POLL, {headers: authHeader()});
-    }
+  static getCategories() {
+    alert(API_BASE_URL + API_URL_POLL + "category");
+    // return axios.get(API_BASE_URL + API_URL_POLL, { headers: authHeader() });
+    return axios.get(API_BASE_URL + API_URL_POLL + "category");
+  }
 }
 
 export default PollService;
